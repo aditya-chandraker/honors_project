bc6bc2c670893ab4eb0328796c8b17c1244d2d17
aditya-chandraker <aditya.chandraker@uconn.com>
2022-11-23 21:57:28 -0500

    finished

--- honorsproject.py
+++ honorsproject.py
@@ -1,58 +1,72 @@
-# importing Packages from tkinter
+# imports
 from tkinter import *
-from tkinter import messagebox
 import random
+from tkinter import messagebox
 
-colors = ["red", "orange", "yellow", "green",
+# Reset the Game
+def reset():
+    global colors, turn, score, button_list, last_index_clicked, score_label, root
+
+    # Create our matches
+    colors = ["red", "orange", "yellow", "green",
           "blue", "purple", "black", "grey"] * 2
+    
+    random.shuffle(colors) # Shuffle our matches
 
-# Button
-b = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-b_true_table = [[False, False, False, False], [False, False, False, False], [
-    False, False, False, False], [False, False, False, False]]
+    root.title('Memory Game')
 
-random.shuffle(colors)
+    score = 20
+    last_index_clicked = None
+    
+    button_list = [Button(height=4, width=8, command=lambda i=i: button_click(i)) for i in range(0,16)]
+    
+    counter = 0
+    for button in button_list:
+        button.grid(row=counter//4, column=counter%4)
+        counter += 1
 
-
-def clicked(r, c):
-    b[r][c].configure(bg=colors[(r*4+c)])
-
-    if not False in b_true_table:
-        pass
-
-
-def play_game():
-
-    # text for buttons
-    for i in range(4):
-        for j in range(4):
-            button = Button(height=4, width=8,
-                            command=lambda r=i, c=j: clicked(r, c))
-            b[i][j] = button
-            b[i][j].grid(row=i, column=j)
-
-    mainloop()
-
-
-def helloCallBack():
-    pass
-
-
-if __name__ == "__main__":
-    # Design window
-    # Creating the Canvas
-    root = Tk()
-    mylabel = Label(text="Score", height=2, width=8,)
+    mylabel = Label(text="Score", height=2, width=8)
     mylabel.grid(row=4, column=0)
-    score = Label(text="20", height=2, width=8,)
-    score.grid(row=4, column=1)
+
+    score_label = Label(text="20", height=2, width=8)
+    score_label.grid(row=4, column=1)
 
     cool_button = Button(
-        text="reset", command=helloCallBack, height=2, width=8,)
+        text="reset", command=lambda: reset(), height=2, width=8,)
     cool_button.grid(row=4, column=3)
 
-    # Title of the window
-    root.title("Memory Game")
-    #root.resizable(0, 0)
 
-    play_game()
+def button_click(index):
+    print(index)
+    global score, last_index_clicked
+
+    button_list[index].configure(bg=colors[index])
+    button_list[index].config(state="disabled")
+
+    if last_index_clicked != None:
+
+        if score <= 1:
+            messagebox.showinfo("You lost", "Congrats!")
+            reset()
+
+        if colors[index] == colors[last_index_clicked]:
+            if all("disabled" in item["state"] for item in button_list):
+                messagebox.showinfo("You won", "Congrats!")
+                reset()
+            
+        else:
+            messagebox.showinfo("Incorrect!", "Incorrect")
+            button_list[index].config(state="active", bg="#F0F0F0")
+            button_list[last_index_clicked].config(state="active", bg="#F0F0F0")
+            score -= 1
+            score_label.configure(text=str(score))
+
+        last_index_clicked = None
+
+    else:
+        last_index_clicked = index
+
+if __name__ == "__main__":
+    root = Tk()
+    reset()
+    root.mainloop()
